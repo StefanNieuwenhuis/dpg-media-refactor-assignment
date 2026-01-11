8c279fe1b0b50427220579832533a3c98c3113e1
Stefan Nieuwenhuis <stefannhs@gmail.com>
2026-01-11 20:59:41 +0100

    add inventory item name constants

--- constants.py
+++ constants.py
@@ -1,8 +1,16 @@
 # --------------------------------------------------
 # Constants
 # --------------------------------------------------
-
 MIN_QUALITY = 0
 MAX_QUALITY = 50
 
 SULFURAS_QUALITY = 80
+
+# --------------------------------------------------
+# Inventory Item Names
+# --------------------------------------------------
+AGED_BRIE = "Aged Brie"
+BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+DEXTERITY_VEST = "+5 Dexterity Vest"
+ELIXER_MONGOOSE = "Elixir of the Mongoose"
+SULFURAS = "Sulfuras, Hand of Ragnaros"
